0c71b10b8ff28f29085b455c80e6375e58e7bf7b
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2023-10-18 22:41:49 -0400

    splash designed and updated

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Carbon Track </Text>
+      <Text> CarbonTrack </Text>
       <StatusBar style="auto" />
     </View>
   );
