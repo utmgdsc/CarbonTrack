fc699f8ef0d405686cc008625b6a59ab640712b0
squishyhoshi <squishyhoshi0208@gmail.com>
2023-10-19 05:00:01 -0400

    Added 2 fonts

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Montserrat: require('./assets/fonts/MontserratThinRegular.ttf'),
+    Josefin: require('./assets/fonts/JosefinSansThinRegular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Carbon Track </Text>
+      <Text style={{ fontFamily: 'Montserrat', fontSize: 30 }}>Carbon Track </Text>
       <StatusBar style="auto" />
     </View>
   );
