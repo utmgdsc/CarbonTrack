dc35d2224fcf7bfbc10012e9c61727b4183c2393
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2023-10-20 21:46:57 -0400

    setting up navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< Updated upstream
-=======
 import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -18,22 +16,14 @@ function HomeScreen() {
 
 const Stack = createNativeStackNavigator();
 
->>>>>>> Stashed changes
 
 export default function App() {
   return (
-<<<<<<< Updated upstream
-    <View style={styles.container}>
-      <Text> CarbonTrack </Text>
-      <StatusBar style="auto" />
-    </View>
-=======
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
->>>>>>> Stashed changes
   );
 }
 
