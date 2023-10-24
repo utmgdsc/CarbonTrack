c0c3bfbca7db7c30183492f506e7109cd5ea12c6
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2023-10-24 19:31:51 -0400

    implemented crud functionality

--- backend/models/user.py
+++ backend/models/user.py
@@ -3,9 +3,11 @@ User Model
 """
 
 from __future__ import annotations
+import json
 from typing import Optional
 from models.abstract_db_model import DB_MODEL
 from bson import ObjectId
+from bson import json_util
 
 
 class User(DB_MODEL):
@@ -16,14 +18,17 @@ class User(DB_MODEL):
         super().__init__(oid)
         self.full_name = str(full_name)
 
-    def to_json(self) -> dict:
-        return {
+    def to_json(self, for_mongodb: bool = False) -> json:
+        res = {
             '_id': self.oid,
             'full_name': self.full_name,
         }
+        if for_mongodb:
+            return res
+        return json.loads(json_util.dumps(res))
 
     @staticmethod
-    def from_json(doc: dict) -> User:
+    def from_json(doc: json) -> User:
         return User(
             oid=ObjectId(doc["_id"]),
             full_name=doc["full_name"],
--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,18 +1,52 @@
 # Python Imports
 import base64
+import json
 from io import BytesIO
+import pprint
 
 import flask
+from bson import ObjectId
 from flask import Blueprint, Response, jsonify, request
 
-# uncomment if you want to populate the db everytime the backend starts
-# from routes.cockroach.populate import populate
-# populate()
+from models.user import User
+from mongodb_api.carbon_track_db import CarbonTrackDB
+from bson import json_util
 
 
 users = Blueprint('/users', __name__)
 
 
-@users.route("/get_user", methods=['POST'])
-def get_treatment(user_id: str) -> Response:
-    pass
+@users.route("/user/<user_id>", methods=['GET'])
+def get_user(user_id: str) -> Response:
+    query = {"_id": ObjectId(user_id)}
+    item = CarbonTrackDB.users_coll.find_one(query)
+    item = User.from_json(item).to_json()
+    return jsonify({'user': item})
+
+
+@users.route("/user", methods=['POST'])
+def create_user() -> Response:
+    user = User.from_json(request.get_json()['user']).to_json(for_mongodb=True)
+    inserted_id = CarbonTrackDB.users_coll.insert_one(user).inserted_id
+    user = User.from_json(CarbonTrackDB.users_coll.find_one({"_id": inserted_id})).to_json()
+    print(inserted_id)
+    return user
+
+@users.route("/user/<user_id>", methods=['DELETE'])
+def delete_user(user_id: str) -> Response:
+    query = {"_id": ObjectId(user_id)}
+    item = CarbonTrackDB.users_coll.find_one(query)
+    item = User.from_json(item).to_json()
+    CarbonTrackDB.users_coll.delete_one(query)
+    return jsonify({'deleted user': item})
+
+
+@users.route("user/<user_id>", methods=["PATCH"])
+def update_user(user_id: str, attr:str, val:str) -> Response:
+    query = {"_id": ObjectId(user_id)}
+    item = CarbonTrackDB.users_coll.update_one(query, {'$set': {attr: val}})
+    item = CarbonTrackDB.users_coll.find_one(query)
+    item = User.from_json(item).to_json()
+    return jsonify({'updated user': item})
+
+
