76504043864bdb9fc39ad7163309e41ed75d1142
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2023-10-26 12:46:17 -0400

    [27] cleaned up sign up page
    ## Describe your changes
    
    Re-initialized sign up page
    Potential error with google icon svg for some reason
    
    ## Trello ticket number and link
    
    [31]  (https://trello.com/c/3hPZxdSM/31-fe-setup-navigation-the-app)
    
    ## Checklist before requesting review
    
    - [ ] I have performed a self-review of my code
    - [ ] I have requested for code review from at least one team mate and our mentore
    -[ ]. I have locally tested my changes and addressed any bugs
    - [ ] All naming standards are followed (i.e branch, commits, title)

--- frontend/src/screens/signup.tsx
+++ frontend/src/screens/signup.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
 import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -21,13 +21,18 @@ export default function SignUp() {
   }
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
+    <View style={{flex: 1, justifyContent: 'center', backgroundColor: '#E0EEC6',}}>
+      
       <View style={{paddingHorizontal: 20}}>
-
-        <View style={{alignItems: 'center',}}>
-          <Text style={{ fontFamily: 'Montserrat', fontSize: 30, marginBottom: 30, fontWeight: '700'}}> Sign Up </Text>
+        <Text style={styles.h1} >Sign Up</Text>
+        <View style={{alignItems: 'center'}}>
+          <TextInput style={styles.txtfielts} placeholder="First Name"/>
+          {/* check out Asynch Storage --https://reactnative.dev/docs/asyncstorage */}
+          <TextInput style={styles.txtfielts} placeholder="Last Name"/>
+          <TextInput style={styles.txtfielts} placeholder="Email"/>
+          <TextInput style={styles.txtfielts} placeholder="Password"/>
+          <TextInput style={styles.txtfielts} placeholder="Re-enter Password"/>
         </View>
-
         <TouchableOpacity style={{backgroundColor: '#2E3E36', padding: 18, borderRadius: 10, marginBottom: 20,}}onPress={() => navigation.navigate('Form')}> 
           <Text style={{textAlign: 'center', fontWeight: '700', fontSize: 16, color: '#fff',}}> Next </Text>
         </TouchableOpacity>
@@ -38,11 +43,33 @@ export default function SignUp() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    // backgroundColor: '#E0EEC6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  txtfielts:{
+    width: '90%',
+    fontSize: 14,
+    borderBlockColor: '#243E36',
+    borderRadius: 10,
+    borderWidth: 2,
+    height: 40,
+    margin: 5,
+    padding: 10,
+    color:'#243E36',
+    backgroundColor: '#fff',
+    // fontFamily: 'Montserrat', 
+ 
+  },
+  h1:{
+    fontSize: 26, 
+    // paddingTop: 0,
+    fontWeight: '700', 
+    marginBottom: 40,
+    fontFamily: 'Montserrat', 
+    // textAlignVertical: 'top',
   
+  }
+});
