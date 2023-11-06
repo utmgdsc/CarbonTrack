196f04e02995580fd35185d5c1baf7c08c33abe6
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-11-05 23:53:57 -0500

    [38] Add frontend eslint check (#14)
    * [38] Add push checks
    
    * [38] Add workflow
    
    * [38] Correct name
    
    * [38] Modify a test file
    
    * [38] Enhance linting rules
    
    * [38] Update linting
    
    * [38] Add final rule

--- frontend/src/screens/home.tsx
+++ frontend/src/screens/home.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import SignUpScreen from './signup';
 import { useNavigation } from '@react-navigation/native';
 import AppNavigation from '../components/appNavigation';
-import { StackNavigationProp } from "@react-navigation/stack";
+import { StackNavigationProp } from '@react-navigation/stack';
 import { Button } from 'react-native';
 import { RootStackParamList } from '../components/types';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -20,34 +20,69 @@ export default function HomeScreen() {
   if (!loaded) {
     return null;
   }
-  
+
   return (
-  <View style={{flex: 1, justifyContent: 'center'}}>
-    <View style={{paddingHorizontal: 20}}>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View style={{ paddingHorizontal: 20 }}>
+        <View style={{ alignItems: 'center' }}>
+          <Text
+            style={{ fontFamily: 'Montserrat', fontSize: 30, fontWeight: '700', marginBottom: 30 }}
+          >
+            {' '}
+            Carbon Track{' '}
+          </Text>
+        </View>
 
-      <View style={{alignItems: 'center',}}>
-        <Text style={{ fontFamily: 'Montserrat', fontSize: 30, fontWeight: '700', marginBottom: 30}}> Carbon Track </Text>
+        <TouchableOpacity
+          style={{ backgroundColor: '#2E3E36', padding: 18, borderRadius: 10, marginBottom: 20 }}
+          onPress={() => navigation.navigate('SignUp')}
+        >
+          <Text
+            style={{
+              fontFamily: 'Montserrat',
+              textAlign: 'center',
+              fontWeight: '700',
+              fontSize: 16,
+              color: '#fff',
+            }}
+          >
+            {' '}
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#fff',
+            padding: 15,
+            borderRadius: 10,
+            marginBottom: 20,
+            borderWidth: 3,
+            borderColor: '#2E3E36',
+          }}
+          onPress={() => navigation.navigate('LogIn')}
+        >
+          <Text
+            style={{
+              fontFamily: 'Montserrat',
+              textAlign: 'center',
+              fontWeight: '700',
+              fontSize: 16,
+              color: '#2E3E36',
+            }}
+          >
+            {' '}
+            Log In
+          </Text>
+        </TouchableOpacity>
       </View>
-
-      <TouchableOpacity style={{backgroundColor: '#2E3E36', padding: 18, borderRadius: 10, marginBottom: 20,}}onPress={() => navigation.navigate('SignUp')}> 
-        <Text style={{fontFamily: 'Montserrat', textAlign: 'center', fontWeight: '700', fontSize: 16, color: '#fff',}}> Sign Up</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={{backgroundColor: '#fff', padding: 15, borderRadius: 10, marginBottom: 20, borderWidth: 3, borderColor: '#2E3E36',}}onPress={() => navigation.navigate('LogIn')}> 
-        <Text style={{fontFamily: 'Montserrat', textAlign: 'center', fontWeight: '700', fontSize: 16, color: '#2E3E36',}}> Log In</Text>
-      </TouchableOpacity>
-      
     </View>
-  </View>
-  
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
