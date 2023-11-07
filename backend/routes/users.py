aefcf9483e6523f0679496b1cbd5cbd7fffdef63
Yazan Armoush <yazan.armoush1@gmail.com>
2023-11-07 16:07:37 -0500

    Update users.py

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -18,7 +18,6 @@ users = Blueprint('/users', __name__)
 
 
 @users.route("/user/<user_id>", methods=['GET'])
-@carbon_auth.auth.login_required
 def get_user(user_id: str) -> Response:
     query = {"_id": ObjectId(user_id)}
     item = CarbonTrackDB.users_coll.find_one(query)
