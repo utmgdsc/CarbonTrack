3f8dec6b48449421acddc3c39a8009d2e9e73b81
squishyhoshi <squishyhoshi0208@gmail.com>
2023-11-11 16:40:36 -0500

    [41] added transportation forum

---
+++ frontend/src/screens/transportationForum.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, } from 'react-native';
+import * as React from 'react';
+import { type StackNavigationProp } from "@react-navigation/stack";
+import { type RootStackParamList } from '../components/types';
+import { useFonts } from 'expo-font';
+
+export type StackNavigation = StackNavigationProp<RootStackParamList>;
+
+export default function TransportationForum(): JSX.Element {
+  const [loaded] = useFonts({
+    Montserrat: require('../../assets/fonts/MontserratThinRegular.ttf'),
+    Josefin: require('../../assets/fonts/JosefinSansThinRegular.ttf'),
+  });
+
+  if (!loaded) {
+    return <></>;
+  }
+  
+  return (
+  <View style={styles.container}>
+      <Text style={styles.header}>
+        Calculate your emissions from transportation:
+      </Text>
+  </View>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: 'center', 
+        paddingHorizontal: 30,
+        backgroundColor: '#E0EEC6'
+    },
+    header: {
+        color: '#2E3E36', 
+        fontFamily: 'Montserrat', 
+        fontSize: 30, 
+        fontWeight: '700',
+        marginBottom: 30,
+      },
+  });
