1e902666a9e6705e50428ec0f9d8c2dab586a5a3
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-11-12 22:38:40 -0500

    [form-management] Add yup, formik, and fix sign up bug (#18)
    * [form-management] Add yup, formik, and fix sign up bug
    
    * [form-management] Make reusable component
    
    * [form-management] Remove unused styles and make backend errors more descriptive

--- frontend/src/screens/signup.tsx
+++ frontend/src/screens/signup.tsx
@@ -1,14 +1,5 @@
-/* eslint-disable react-native/no-unused-styles */
 import * as React from 'react';
-import { useState } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-  Alert,
-} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { useFonts } from 'expo-font';
 import { type RootStackParamList } from '../components/types';
 import { type StackNavigationProp } from '@react-navigation/stack';
@@ -17,49 +8,30 @@ import Colors from '../../assets/colorConstants';
 import firebaseService from '../utilities/firebase';
 import { createUser } from '../APIs/UsersAPI';
 import ObjectID from 'bson-objectid';
-
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import FormTextField from '../components/forms/formTextField';
 export type StackNavigation = StackNavigationProp<RootStackParamList>;
 
-export default function SignUp(): JSX.Element{
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [verifiedPassword, setVerifiedPassword] = useState('');
+interface ISignUpFields {
+  fullName: string;
+  email: string;
+  password: string;
+}
+
+const SignUpSchema = Yup.object().shape({
+  fullName: Yup.string().required('Full Name is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+  repeatPassword: Yup.string()
+    .oneOf([Yup.ref('password')], 'Passwords must match')
+    .required('Password confirmation is required'),
+});
+
+export default function SignUp(): JSX.Element {
   const navigation = useNavigation<StackNavigation>();
 
-  const checkInput = (): boolean => {
-    if (email.trim().length === 0) {
-      Alert.alert('Please Enter a Valid Email');
-      return false;
-    } else if (password.trim().length === 0) {
-      Alert.alert('Please Enter a Valid Password');
-      return false;
-    } else if (password !== verifiedPassword) {
-      Alert.alert('Please correctly rewrite your Password');
-      return false;
-    }else {
-      return true;
-    }
-  };
-
-  const handleSignUp = async (): Promise<void> => {
-    if (checkInput()) {
-      try {
-        await firebaseService.createUser(email, password);
-        await firebaseService.createUser(email, verifiedPassword);
-        await createUser({
-          _id: new ObjectID,
-          full_name: fullName,
-          email
-        });
-        navigation.navigate('DashBoard');
-      } catch (error) {
-        console.error('Error when creating User:', error);
-      }
-    }
-
-  };
-
+  // TODO: fonts should be loaded at the global level, and not at the component level.
   const [loaded] = useFonts({
     Montserrat: require('../../assets/fonts/MontserratThinRegular.ttf'),
     Josefin: require('../../assets/fonts/JosefinSansThinRegular.ttf'),
@@ -69,146 +41,114 @@ export default function SignUp(): JSX.Element{
     return <></>;
   }
 
+  const handleSignUp = async (fields: ISignUpFields): Promise<void> => {
+    const { fullName, email, password } = fields;
+    try {
+      await firebaseService.createUser(email, password);
+      await createUser({
+        _id: new ObjectID(),
+        full_name: fullName,
+        email,
+      });
+      console.log('User was created succesfully:', email);
+      navigation.navigate('DashBoard');
+    } catch (error) {
+      console.error('Error when creating User:', error);
+    }
+  };
+
   return (
-    <View style={styles.headerContainer}>
-    <View style={styles.headerBox}>
-      <Text style={styles.header}> Sign Up </Text>
+    <>
+      <Formik
+        initialValues={{ fullName: '', email: '', password: '', repeatPassword: '' }}
+        validationSchema={SignUpSchema}
+        onSubmit={async (values) => await handleSignUp(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+          <View style={styles.headerContainer}>
+            <View style={styles.headerBox}>
+              <Text style={styles.header}>Sign Up</Text>
 
-      <View style={styles.textbox}>
-        <TextInput
-            style={styles.textInputBox}
-            placeholder="Full Name"
-            value={fullName}
-            onChangeText={(fullName) => {setFullName(fullName)}}
-          />
-      </View>
+              <FormTextField
+                placeholder="Full Name"
+                handleChange={handleChange('fullName')}
+                handleBlur={handleBlur('fullName')}
+                value={values.fullName}
+                touchedValue={touched.fullName}
+                errorValue={errors.fullName}
+                secureTextEntry={false}
+              />
 
-      <View style={styles.textbox}>
-      <TextInput
-        style={styles.textInputBox}
-        placeholder="Email"
-        value={email}
-        onChangeText={(email) => {setEmail(email)}}
-      />
-      </View>
-      <View style={styles.textbox}>
-      <TextInput
-        style={styles.textInputBox}
-        placeholder="Password"
-        value={password}
-        onChangeText={(password) => {setPassword(password)}}
-      />
-      </View>
-      <View style={styles.textbox}>
-      <TextInput 
-        style={styles.textInputBox} 
-        placeholder="Re-enter Password"
-        value={verifiedPassword}
-        onChangeText={(verifiedPassword) => {setVerifiedPassword(verifiedPassword)}} />
-      </View>
+              <FormTextField
+                placeholder="Email Address"
+                handleChange={handleChange('email')}
+                handleBlur={handleBlur('email')}
+                value={values.email}
+                touchedValue={touched.email}
+                errorValue={errors.email}
+                secureTextEntry={false}
+              />
 
-      <TouchableOpacity
-          style={styles.buttoning}
-          onPress={ () => {void handleSignUp();
-          }}
-        
-        >
-          <Text style={styles.altContainerText}> Next </Text>
-        </TouchableOpacity>
-    </View>
-  </View>   
+              <FormTextField
+                placeholder="Password"
+                handleChange={handleChange('password')}
+                handleBlur={handleBlur('password')}
+                value={values.password}
+                touchedValue={touched.password}
+                errorValue={errors.password}
+                secureTextEntry={true}
+              />
+
+              <FormTextField
+                placeholder="Repeat Password"
+                handleChange={handleChange('repeatPassword')}
+                handleBlur={handleBlur('repeatPassword')}
+                value={values.repeatPassword}
+                touchedValue={touched.repeatPassword}
+                errorValue={errors.repeatPassword}
+                secureTextEntry={true}
+              />
+
+              <TouchableOpacity style={styles.buttoning} onPress={() => handleSubmit()}>
+                <Text style={styles.altContainerText}>Next</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </Formik>
+    </>
   );
 }
 
+// TODO: Make a global stylesheet where you have your primary, secondary, button styles, so
+// you don't redefine them every time. DRY principle.
+
 const styles = StyleSheet.create({
-  altContainer: {
-    alignItems: 'center',
-    backgroundColor: Colors.LIGHTFGREEN,
-    flexDirection: 'row', 
-    marginBottom: 30,
-  },
-    
-  altContainerFloater:{
-    backgroundColor: Colors.DARKLIMEGREEN,
-    flex: 1, 
-    height: 1, 
-  },
-  altContainerSub: {
-    backgroundColor: Colors.DARKLIMEGREEN,
-    flex: 1, 
-    height: 1, 
-  },
   altContainerText: {
     color: Colors.DARKLIMEGREEN,
     fontFamily: 'Montserrat',
     fontSize: 18,
     marginHorizontal: 5,
-    textAlign: 'center',  
+    textAlign: 'center',
   },
-  buttoning:{
-    backgroundColor: Colors.DARKGREEN, 
+  buttoning: {
+    backgroundColor: Colors.DARKGREEN,
     borderRadius: 10,
     marginBottom: 20,
-    padding: 18,  
-  }, 
-  buttoningText: {
-    color: Colors.WHITE, 
-    fontSize: 16, 
-    fontWeight: '700', 
-    textAlign: 'center', 
-  },
-  footer: {
-    color: Colors.DARKLIMEGREEN,
-    fontFamily: 'Montserrat',
-    fontSize: 18,
-    marginBottom: 30,
-    textAlign: 'center',
-  },
-  footerBold: {
-    color: Colors.DARKGREEN,
-    fontFamily: 'Montserrat',
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 30,
-    textAlign: 'center',
-  },
-  footerContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'center'
-  },
-  googleIcon:{
-    alignItems: 'center', 
-    justifyContent: 'center'
+    padding: 18,
   },
   header: {
-    color: Colors.DARKGREEN, 
-    fontFamily: 'Montserrat', 
-    fontSize: 30, 
+    color: Colors.DARKGREEN,
+    fontFamily: 'Montserrat',
+    fontSize: 30,
     fontWeight: '700',
     marginBottom: 30,
   },
   headerBox: {
-    paddingHorizontal: 30
+    paddingHorizontal: 30,
   },
-  headerContainer:{
+  headerContainer: {
     flex: 1,
-    justifyContent: 'center'
-  },
-  icon: {
-    height: 24,
-    marginRight: 5,
-    width: 24,
-  },
-  textInputBox: {
-    flex:1, 
-    paddingVertical:0,
-  },
-  textbox: {
-    borderBottomColor: Colors.GREY,
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-    marginBottom: 25,
-    paddingBotton: 8,
+    justifyContent: 'center',
   },
 });
-  
