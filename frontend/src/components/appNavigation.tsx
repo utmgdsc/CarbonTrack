b92a26f332d5910b52c15857727c156547b202f4
squishyhoshi <squishyhoshi0208@gmail.com>
2023-11-17 05:29:47 -0500

    added monthly history tracker with chart

--- frontend/src/components/appNavigation.tsx
+++ frontend/src/components/appNavigation.tsx
@@ -10,16 +10,16 @@ import Forum from '../screens/forum';
 import FoodForum from '../screens/foodForum';
 import EnergyForum from '../screens/energyForum';
 import TransportationForum from '../screens/transportationForum';
-
+import FoodHistory from '../screens/foodHistory';
+import TransportationHistory from '../screens/transportationHistory';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-
- const AppNavigation = ():JSX.Element => {
+const AppNavigation = (): JSX.Element => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={HomeScreen}  />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="LogIn" component={LogInScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="DashBoard" component={DashBoardScreen} />
@@ -27,9 +27,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
         <Stack.Screen name="FoodForum" component={FoodForum} />
         <Stack.Screen name="EnergyForum" component={EnergyForum} />
         <Stack.Screen name="TransportationForum" component={TransportationForum} />
+        <Stack.Screen name="FoodHistory" component={FoodHistory} />
+        <Stack.Screen name="TransportationHistory" component={TransportationHistory} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default AppNavigation;
