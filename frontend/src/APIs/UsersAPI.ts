ddac02fc6e6ad2e5f55e41fd363f41347f579cd7
Yazan Armoush <yazan.armoush1@gmail.com>
2023-11-22 14:20:17 -0500

    Added User Methods

--- frontend/src/APIs/UsersAPI.ts
+++ frontend/src/APIs/UsersAPI.ts
@@ -5,59 +5,62 @@ import firebaseService from '../utilities/firebase';
 
 const routeName = '/users';
 
-export const GetLoggedInUser = async (): Promise<undefined | User> => {
-  const firebaseUser = await firebaseService.getFirebaseUser();
-  if (firebaseUser != null) {
-    if (firebaseUser.email != null) {
-      return await getUserByEmail(firebaseUser.email);
+export const UsersAPI = {
+
+  GetLoggedInUser: async () => {
+    const firebaseUser = await firebaseService.getFirebaseUser();
+    if (firebaseUser != null) {
+      if (firebaseUser.email != null) {
+        return await UsersAPI.getUserByEmail(firebaseUser.email);
+      }
+    }
+  },
+
+  getUser: async (userID: ObjectID)=> {
+    try {
+      const res = await FLASK_HTTPS.get(routeName + '/user/' + userID.str);
+      return res.data.user as User;
+    } catch (error) {
+      console.error('Error fetching user from Flask BE: ', error);
+      console.error('Temp tip: have you started the backend?: ');
+      return undefined;
+    }
+  },
+  
+  getUserByEmail: async (email: string)=> {
+    try {
+      const res = await FLASK_HTTPS.get(routeName + '/user_email/' + email);
+      return res.data.user as User;
+    } catch (error) {
+      console.error('Error fetching user from Flask BE: ', error);
+      console.error('Temp tip: have you started the backend?: ');
+      return undefined;
+    }
+  },
+  
+  createUser: async (user: User)=> {
+    try {
+      const res = await FLASK_HTTPS.put(routeName + '/user', {
+        user,
+      });
+      return res.data.user as User;
+    } catch (error) {
+      console.error('Error creating user in Flask BE: ', error);
+      console.error('Temp tip: have you started the backend?: ');
+      return undefined;
+    }
+  },
+  
+  updateUser: async (user: User)=> {
+    try {
+      const res = await FLASK_HTTPS.patch(routeName + '/user/' + user._id.str, {
+        user,
+      });
+      return res.data.user as User;
+    } catch (error) {
+      console.error('Error fetching user from Flask BE: ', error);
+      console.error('Temp tip: have you started the backend?: ');
+      return undefined;
     }
   }
-};
-
-export const getUser = async (userID: ObjectID): Promise<undefined | User> => {
-  try {
-    const res = await FLASK_HTTPS.get(routeName + '/user/' + userID.str);
-    return res.data.user as User;
-  } catch (error) {
-    console.error('Error fetching user from Flask BE: ', error);
-    console.error('Temp tip: have you started the backend?: ');
-    return undefined;
-  }
-};
-
-export const getUserByEmail = async (email: string): Promise<undefined | User> => {
-  try {
-    const res = await FLASK_HTTPS.get(routeName + '/user_email/' + email);
-    return res.data.user as User;
-  } catch (error) {
-    console.error('Error fetching user from Flask BE: ', error);
-    console.error('Temp tip: have you started the backend?: ');
-    return undefined;
-  }
-};
-
-export const createUser = async (user: User): Promise<undefined | User> => {
-  try {
-    const res = await FLASK_HTTPS.put(routeName + '/user', {
-      user,
-    });
-    return res.data.user as User;
-  } catch (error) {
-    console.error('Error creating user in Flask BE: ', error);
-    console.error('Temp tip: have you started the backend?: ');
-    return undefined;
-  }
-};
-
-export const updateUser = async (user: User): Promise<undefined | User> => {
-  try {
-    const res = await FLASK_HTTPS.patch(routeName + '/user/' + user._id.str, {
-      user,
-    });
-    return res.data.user as User;
-  } catch (error) {
-    console.error('Error fetching user from Flask BE: ', error);
-    console.error('Temp tip: have you started the backend?: ');
-    return undefined;
-  }
-};
+}
