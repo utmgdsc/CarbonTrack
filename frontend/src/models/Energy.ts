1b7d476fe818aa8cb05c0888325b53e86f221a98
Yazan Armoush <yazan.armoush1@gmail.com>
2023-11-27 16:14:24 -0500

    Updated Models

--- frontend/src/models/Energy.ts
+++ frontend/src/models/Energy.ts
@@ -1,13 +1,13 @@
 import type ObjectID from 'bson-objectid';
 
 export interface Energy {
-  _id: ObjectID;
-  user_id: ObjectID;
-  heating_oil: number;
-  natural_gas: number;
-  electricity: number;
-  province: string;
-  household: number;
-  carbon_emissions: number;
-  date: Date;
+  _id: ObjectID
+  user_id: ObjectID
+  carbon_emissions: number
+  date: Date
+  heating_oil: number
+  natural_gas: number
+  electricity: number
+  province: string
+  household: number
 }
--- frontend/src/models/Transportation.ts
+++ frontend/src/models/Transportation.ts
@@ -4,13 +4,13 @@ import { type ObjectId } from "mongodb";
 export interface TransportationEntry {
     _id: ObjectId
     user_id: ObjectId
+    carbon_emissions: number
+    date: Date
     bus: number
     train: number
     motorbike: number
     electric_car: number
     gasoline_car: number
-    carbon_emissions: number
-    date: Date
 }
 
 export interface MonthlyEntry {
