1b30c2b112fa7c3d99df747879a8131f45f4ad3e
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2023-12-16 01:27:18 -0500

    settinfs ui commit 1
    fuel efficiency update work
    all settings page basic makeover done

--- frontend/src/screens/settings/updateHomeScreen.tsx
+++ frontend/src/screens/settings/updateHomeScreen.tsx
@@ -22,8 +22,13 @@ export default function UpdateHomeScreen(): JSX.Element {
 
   const [newProvince, setNewProvince] = useState<string>('');
   const [newOccupancy, setNewOccupancy] = useState<number>(0);
+  const [newFuelEfficeincy, setnewFuelEfficeincy] = useState<number>(0);
   const [user, setUser] = useState<User | undefined>(undefined);
   const [userid, setUserid] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [isFocused2, setIsFocused2] = useState<boolean>(false);
+  const [isFocused3, setIsFocused3] = useState<boolean>(false);
+
 
 
   const navigation = useNavigation<StackNavigation>();
@@ -46,43 +51,73 @@ export default function UpdateHomeScreen(): JSX.Element {
   }, []);
   console.log(userid)
 
+  const handleFocus = ():void  => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = ():void => {
+    setIsFocused(false);
+  };
+  const handleFocus2 = ():void  => {
+    setIsFocused2(true);
+  };
+
+  const handleBlur2 = ():void => {
+    setIsFocused2(false);
+  };
+
+  const handleFocus3 = ():void  => {
+    setIsFocused3(true);
+  };
+
+  const handleBlur3 = ():void => {
+    setIsFocused3(false);
+  };
 
   const handleUpdateHome = async (): Promise<void> => {
     try {
       console.log('Updating home info');
-      console.log(newProvince)
-      console.log(newOccupancy)
   
-      if (newProvince !== '' && user !== undefined) {
-        console.log('Updating province');
-        const updatedProvincialUser = await UsersAPI.updateUserProvince(user, newProvince);
+      if (user !== undefined) {
+        let updatedUser: User | undefined;
   
-        console.log('Updated user:', updatedProvincialUser);
-  
-        if (updatedProvincialUser != null) {
-          setUser(updatedProvincialUser);
-          console.log('Updated province');
-        }
-      }
-  
-      if (newOccupancy !== undefined && user !== undefined) {
-        console.log('Updating occupancy');
-        const updatedOccupancyUser = await UsersAPI.updateUserOccupancy(user, newOccupancy);
-  
-        console.log('Updated user:', updatedOccupancyUser);
-  
-        if (updatedOccupancyUser != null) {
-          setUser(updatedOccupancyUser);
-          console.log('Updated occupancy');
+        if (newProvince !== '' && newProvince !== user.province) {
+          console.log('Updating province');
+          updatedUser = await UsersAPI.updateUserProvince(user, newProvince);
+          console.log('Updated province:', updatedUser?.province);
         }
   
-        console.log('User household:', updatedOccupancyUser?.household);
+        if (newOccupancy !== 0 && newOccupancy !== user.household) {
+          console.log('Updating occupancy');
+          updatedUser = await UsersAPI.updateUser({
+            ...user,
+            household: newOccupancy,
+          });
+          console.log('Updated occupancy:', updatedUser?.household);
+        }
+  
+        if (newFuelEfficeincy !== 0 && newFuelEfficeincy !== user.fuel_efficiency) {
+          console.log('Updating fuel efficiency');
+          updatedUser = await UsersAPI.updateUser({
+            ...user,
+            fuel_efficiency: newFuelEfficeincy,
+          });
+          console.log('Updated fuel efficiency:', updatedUser?.fuel_efficiency);
+        }
+  
+        if (updatedUser !== undefined) {
+          setUser(updatedUser);
+          console.log('User updated:', updatedUser);
+        }
       }
     } catch (e) {
       console.error('Updating Home Info error occurred:', e);
     }
     return await Promise.resolve();
   };
+  
+  
+  
 
   if (!loaded) {
     return <></>;
@@ -98,24 +133,45 @@ export default function UpdateHomeScreen(): JSX.Element {
         <Text style={styles.header}> Update Home Info </Text>
       </View>
       <View style={styles.profileContainer}>
-        <View style={styles.textInputBox}>
-          <Text style={styles.label}>How many people live in your home?:</Text>
-          <TextInput
-            keyboardType="numeric"
-            placeholder= {'Currently: '+String(user?.household)}
-            placeholderTextColor={Colors.WHITE}
-            style={styles.textInput}
-            onChangeText={(number) => setNewOccupancy(parseInt(number))}
-          />
-          <Text style={styles.label}>Your Province:</Text>
-          <TextInput
-            
-            placeholder={'Currently: '+ String(user?.province)}
-            placeholderTextColor={Colors.WHITE}
-            style={styles.textInput}
-            onChangeText={(text) => setNewProvince(text)}
-          />
+        
+        <View style={styles.textInputFields}>
+          <View style={styles.textInputBox}>
+            <Text style={styles.label}>How many people live in your home?:</Text>
+            <TextInput
+              keyboardType="numeric"
+              placeholder= {'Currently: '+String(user?.household)}
+              placeholderTextColor={Colors.LIGHTBLACK}
+              onChangeText={(number) => setNewOccupancy(parseInt(number))}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              style={[styles.textInput, isFocused && styles.activeTextInput]}
+            />
+          </View>
+          <View style={styles.textInputBox}>
+            <Text style={styles.label}>Your Province:</Text>
+            <TextInput
+              placeholder={'Currently: '+ String(user?.province)}
+              placeholderTextColor={Colors.LIGHTBLACK}
+              onChangeText={(text) => setNewProvince(text)}
+              onFocus={handleFocus2}
+              onBlur={handleBlur2}
+              style={[styles.textInput, isFocused2 && styles.activeTextInput]}
+            />
+          </View>
+          <View style={styles.textInputBox}>
+            <Text style={styles.label}>Your New Fuel Efficiency:</Text>
+            <TextInput
+              placeholder={'Currently: '+ String(user?.fuel_efficiency)}
+              placeholderTextColor={Colors.LIGHTBLACK}
+              onChangeText={(number) => setnewFuelEfficeincy(parseInt(number))}
+              onFocus={handleFocus3}
+              onBlur={handleBlur3}
+              style={[styles.textInput, isFocused3 && styles.activeTextInput]}
+            />
+          </View>
         </View>
+
+
         <TouchableOpacity style={styles.saveButton} onPress={()  => { void handleUpdateHome()}}>
           <Text style={styles.saveButtonText}> Confirm Update </Text>
         </TouchableOpacity>
@@ -124,74 +180,161 @@ export default function UpdateHomeScreen(): JSX.Element {
   );
 }
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: Colors.DARKDARKGREEN,
-  },
-  profileContainer: {
-    height: 645,
-    width: '85%',
-    backgroundColor: Colors.DARKGREEN,
-    borderRadius: 20,
-    alignSelf: 'center',
-    margin: 40,
+  // Existing styles
+  activeTextInput: {
+    borderColor: Colors.LIGHTGREENBUTTON,
+    borderWidth: 2,
   },
   backButton: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    left: 20,
+    padding: 10,
     position: 'absolute',
     top: 60,
-    left: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
   },
   buttonText: {
-    fontSize: 16,
     color: Colors.WHITE,
+    fontSize: 16,
     fontWeight: '500',
   },
+  container: {
+    backgroundColor: Colors.DARKDARKGREEN,
+    flexGrow: 1,
+  },
   header: {
-    fontSize: 40,
     color: Colors.WHITE,
-    position: 'absolute',
-    top: 105,
-    textAlign: 'center',
+    fontSize: 36,
     fontWeight: '700',
+    position: 'absolute',
+    textAlign: 'center',
+    top: 105,
   },
   headerBox: {
     alignItems: 'center',
     paddingBottom: 130,
   },
-  textInputBox: {
-    alignSelf: 'center',
-    top: '10%',
-    padding: 10,
-  },
   label: {
-    fontSize: 16,
-    opacity: 0.5,
-    color: Colors.WHITE,
+    color: Colors.LIGHTFGREEN,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  profileContainer: {
+    backgroundColor: Colors.DARKLIGHTDARKGREEN,
+    borderRadius: 20,
+    height: 645,
+    margin: 40,
+    alignSelf: 'center',
+    width: '85%',
   },
   saveButton: {
-    borderRadius: 5,
-    alignSelf: 'center',
-    top: '20%',
-  },
-  textInput: {
-    paddingHorizontal: 5,
-    marginBottom: 30,
-    marginTop: 10,
-    borderRadius: 10,
-    fontSize: 16,
-    borderBottomWidth: 1,
-    borderColor: Colors.WHITE,
-    width: 270,
-    color: Colors.WHITE,
+    backgroundColor: Colors.TRANSGREENLOGOUT,
+    borderRadius: 8,
+    width: 140,
+    height: 40,
+    left: '50%',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+    marginVertical: 25,
   },
   saveButtonText: {
+    color: Colors.WHITE,
+    fontSize: 15,
+    fontWeight: '500',
+    textAlign: 'center'
+    
+  },
+  textInput: {
+    backgroundColor: Colors.DARKDARKGREEN,
+    borderColor: Colors.BORDERGREEN,
+    borderRadius: 8,
+    borderWidth: 1,
     color: Colors.LIGHTFGREEN,
     fontSize: 16,
-    textDecorationLine: 'underline',
-    fontWeight: '400',
-    shadowColor: Colors.LIGHTFGREEN,
+    marginBottom: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
   },
+  textInputBox: {
+    alignSelf: 'center',
+    marginBottom: 50,
+    width: '80%',
+  },
+  textInputFields:{
+    top: '9%'
+  },
+    
+
 });
+// const styles = StyleSheet.create({
+//   container: {
+// d   flexGrow: 1,
+//     backgroundColor: Colors.DARKDARKGREEN,
+//   },
+//   profileContainer: {
+//     height: 645,
+//     width: '85%',
+//     backgroundColor: Colors.DARKGREEN,
+//     borderRadius: 20,
+//     alignSelf: 'center',
+//     margin: 40,
+//   },
+//   backButton: {
+//     position: 'absolute',
+//     top: 60,
+//     left: 20,
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     padding: 10,
+//   },
+//   buttonText: {
+//     fontSize: 16,
+//     color: Colors.WHITE,
+//     fontWeight: '500',
+//   },
+//   header: {
+//     fontSize: 40,
+//     color: Colors.WHITE,
+//     position: 'absolute',
+//     top: 105,
+//     textAlign: 'center',
+//     fontWeight: '700',
+//   },
+//   headerBox: {
+//     alignItems: 'center',
+//     paddingBottom: 130,
+//   },
+//   textInputBox: {
+//     alignSelf: 'center',
+//     top: '10%',
+//     padding: 10,
+//   },
+//   label: {
+//     fontSize: 16,
+//     opacity: 0.5,
+//     color: Colors.WHITE,
+//   },
+//   saveButton: {
+//     borderRadius: 5,
+//     alignSelf: 'center',
+//     top: '20%',
+//   },
+//   textInput: {
+//     paddingHorizontal: 5,
+//     marginBottom: 30,
+//     marginTop: 10,
+//     borderRadius: 10,
+//     fontSize: 16,
+//     borderBottomWidth: 1,
+//     borderColor: Colors.WHITE,
+//     width: 270,
+//     color: Colors.WHITE,
+//   },
+//   saveButtonText: {
+//     color: Colors.LIGHTFGREEN,
+//     fontSize: 16,
+//     textDecorationLine: 'underline',
+//     fontWeight: '400',
+//     shadowColor: Colors.LIGHTFGREEN,
+//   },
+// });
--- frontend/src/screens/settings/updatePassword.tsx
+++ frontend/src/screens/settings/updatePassword.tsx
@@ -21,7 +21,10 @@ import {
   updatePassword,
   EmailAuthProvider,
   reauthenticateWithCredential,
+  verifyBeforeUpdateEmail,
 } from 'firebase/auth';
+import { UsersAPI } from '../../APIs/UsersAPI';
+import { type User } from '../../models/User';
 export type StackNavigation = StackNavigationProp<RootStackParamList>;
 
 export default function UpdateProfileScreen(): JSX.Element {
@@ -31,30 +34,86 @@ export default function UpdateProfileScreen(): JSX.Element {
     Montserrat: require('../../../assets/fonts/MontserratThinRegular.ttf'),
     Josefin: require('../../../assets/fonts/JosefinSansThinRegular.ttf'),
   });
-  const [newPass, setNewPass] = useState<string | undefined>('');  
+  const [newPass, setNewPass] = useState<string>('');
+  const [loggedUser, setLoggedUser] = useState<User | undefined>(undefined);
+  const [newEmail, setNewEmail] = useState<string>(''); 
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+
+  
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user === null) {
+      navigation.navigate('Home');
+    }
+  });
 
   useEffect(() => {
-    const checkAuthState = async (): Promise<void> => {
-      const currentUser = await firebaseService.getFirebaseUser();
-      if (currentUser === null) {
-        navigation.navigate('LogIn');
-      }
+    const fetchUserData = async (): Promise<void> => {
+      void UsersAPI.GetLoggedInUser().then((res) => {
+        if (res != null) {
+          setLoggedUser(res);
+        }
+      });
     };
+    void fetchUserData();
+  }, []);
 
-    const unsubscribe = onAuthStateChanged(getAuth(), (currentUser) => {
-      if (currentUser === null) {
-        navigation.navigate('LogIn');
-      } else {
-        void checkAuthState();
+
+  // useEffect(() => {
+  //   const checkAuthState = async (): Promise<void> => {
+  //     const currentUser = await firebaseService.getFirebaseUser();
+  //     if (currentUser === null) {
+  //       navigation.navigate('LogIn');
+  //     }
+  //   };
+
+  //   const unsubscribe = onAuthStateChanged(getAuth(), (currentUser) => {
+  //     if (currentUser === null) {
+  //       navigation.navigate('LogIn');
+  //     } else {
+  //       void checkAuthState();
+  //     }
+  //   });
+
+  //   return () => {
+  //     unsubscribe(); // Cleanup on component unmount
+  //   };
+  // }, [navigation]);
+
+  const handleUpdateEmail = async (): Promise<void> => {
+    try {
+      const user = await firebaseService.getFirebaseUser();
+
+      if (user != null) {
+        const userCreds = await promptUserForCredentials();
+
+        if (userCreds != null && user.email != null) {
+          const entireCreds = EmailAuthProvider.credential(user.email, userCreds.password);
+          await reauthenticateWithCredential(user, entireCreds).then(async (result) => {
+            console.log('Email (should be) sent! Check inbox. ');
+
+            // email update
+            await verifyBeforeUpdateEmail(user, newEmail);
+            
+
+            // TODO: when Error (auth/missing-new-email).] --make alert (email up to date)
+            Alert.alert(
+              "Check Your Inbox!",`A verification link has been sent to ${newEmail}! Click the link to verify your email. You will now be looged off.`
+            );
+            
+            await firebaseService.signOutUser();
+
+            navigation.navigate('Home');
+          });
+        }
       }
-    });
-
-    return () => {
-      unsubscribe(); // Cleanup on component unmount
-    };
-  }, [navigation]);
-
-
+    } catch (error: any) {
+      // Log any errors
+      console.error('Error updating email in UpdateProfile: ', error);
+    }
+  };
+  
   const handleUpdatePassword = async ():Promise<void> => {
     try {
       const user = await firebaseService.getFirebaseUser();
@@ -71,7 +130,7 @@ export default function UpdateProfileScreen(): JSX.Element {
               // after reauthing, update password
               try {
                 await updatePassword(user, newPass);
-                alert('Password reset email has been sent. Please check your inbox for next steps. You will now be logged out.');
+                Alert.alert('Password Updated Successfully!','You will now be signed out.');
                 console.log('Firebase (frontend): Update password success (maybe)');
                 // sign out user after changing password
                 await firebaseService.signOutUser();
@@ -79,7 +138,7 @@ export default function UpdateProfileScreen(): JSX.Element {
               } catch (updateError: any) {
                 console.error('Updating password error:', updateError);
                 if (updateError.code === "auth/weak-password") {
-                  Alert.alert('Invalid Password', 'Please make sure your password is strong enough.');
+                  Alert.alert('Password Too Weak', 'Please make sure your password is at least 6 characters long.');
                 }
               }
             } catch (reauthError: any) {
@@ -129,93 +188,184 @@ export default function UpdateProfileScreen(): JSX.Element {
           <Ionicons name="arrow-back" size={30} color={Colors.WHITE} />
           <Text style={styles.buttonText}> Settings </Text>
         </TouchableOpacity>
-        <Text style={styles.header}> Update Pasword </Text>
+        <Text style={styles.header}> Update Log In Info </Text>
       </View>
       <View style={styles.profileContainer}>
+
+
+      <View style={styles.textInputFields}>
         <View style={styles.textInputBox}>
+          <Text style={styles.label}>New Email:</Text>
+            <TextInput
+              placeholder={loggedUser?.email}
+              style={[styles.textInput, newEmail.length > 0 && styles.activeTextInput]}
+              onChangeText={(text) => setNewEmail(text)}
+              placeholderTextColor={Colors.LIGHTBLACK}
+            />
+          <TouchableOpacity 
+          style={styles.saveButton} 
+          // onPress={() => {void handleUpdateEmail()}}
+          onPress={() => {
+            void (newEmail.length > 0 && handleUpdateEmail());
+          }}
+          disabled={newEmail.length === 0} 
+>
+            <Text style={styles.saveButtonText}> Update Email </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.passInputBox}>
           <Text style={styles.label}>New Password:</Text>
           <TextInput
             placeholder="New Password"
-            style={styles.textInput}
+            style={[styles.textInput, newPass?.length > 0 && styles.activeTextInput]}
             onChangeText={(text) => setNewPass(text)}
-          />
+            placeholderTextColor={Colors.LIGHTBLACK}
+            secureTextEntry={!isPasswordVisible}
+            />
+          <TouchableOpacity
+            style={styles.passwordVisibilityButton}
+            onPress={() => { setIsPasswordVisible(!isPasswordVisible) }}
+          >
+            <Ionicons
+              name={isPasswordVisible ? 'eye-off' : 'eye'}
+              size={24}
+              color={Colors.TRANSGREENLOGOUT}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+            style={styles.saveButton} 
+            onPress={()  => { void handleUpdatePassword()}}>
+            <Text style={styles.saveButtonText}> Update Password </Text>
+          </TouchableOpacity>
+      </View>
+
+
+      </View>
+
+{/* 
+      <View style={styles.textInputFields}>
+        <View style={styles.textInputBox}>
+          <Text style={styles.label}>New Password:</Text>
+            <TextInput
+              placeholder='New Password'
+              style={[styles.textInput, newPass.length > 0 && styles.activeTextInput]}
+              onChangeText={(text) => setNewPass(text)}
+              placeholderTextColor={Colors.LIGHTBLACK}
+            />
+          <TouchableOpacity 
+          style={styles.saveButton} 
+          onPress={() => {
+            void (newPass.length > 0 && handleUpdatePassword());
+          }}
+          disabled={newPass.length === 0} 
+>
+            <Text style={styles.saveButtonText}> Update Password </Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.saveButton} onPress={()  => { void handleUpdatePassword()}}>
-          <Text style={styles.saveButtonText}> Update Password </Text>
-        </TouchableOpacity>
+      </View> */}
+
       </View>
     </ScrollView>
   );
 }
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: Colors.DARKDARKGREEN,
-  },
-  profileContainer: {
-    height: 645,
-    width: '85%',
-    backgroundColor: Colors.DARKGREEN,
-    borderRadius: 20,
-    alignSelf: 'center',
-    margin: 40,
+  // Existing styles
+  activeTextInput: {
+    borderColor: Colors.LIGHTGREENBUTTON,
+    borderWidth: 2,
   },
   backButton: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    left: 20,
+    padding: 10,
     position: 'absolute',
     top: 60,
-    left: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
   },
   buttonText: {
-    fontSize: 16,
     color: Colors.WHITE,
+    fontSize: 16,
     fontWeight: '500',
   },
+  container: {
+    backgroundColor: Colors.DARKDARKGREEN,
+    flexGrow: 1,
+  },
   header: {
-    fontSize: 40,
     color: Colors.WHITE,
-    position: 'absolute',
-    top: 105,
-    textAlign: 'center',
+    fontSize: 36,
     fontWeight: '700',
+    position: 'absolute',
+    textAlign: 'center',
+    top: 105,
   },
   headerBox: {
     alignItems: 'center',
     paddingBottom: 130,
   },
-  textInputBox: {
-    alignSelf: 'center',
-    top: '10%',
-    padding: 10,
-  },
   label: {
-    fontSize: 16,
-    opacity: 0.5,
-    color: Colors.WHITE,
+    color: Colors.LIGHTFGREEN,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  passwordVisibilityButton: {
+    zIndex: 1, 
+    position: 'absolute',
+    right: 10,
+  },
+  // d passwordVisibilityButton: {
+  //   zIndex: 1, 
+  //   position: 'absolute',
+  //   right: 10,
+  // },
+  profileContainer: {
+    backgroundColor: Colors.DARKLIGHTDARKGREEN,
+    borderRadius: 20,
+    height: 645,
+    margin: 40,
+    alignSelf: 'center',
+    width: '85%',
   },
   saveButton: {
-    borderRadius: 5,
-    alignSelf: 'center',
-    top: '20%',
-  },
-  textInput: {
-    paddingHorizontal: 5,
-    marginBottom: 30,
-    marginTop: 10,
-    borderRadius: 10,
-    fontSize: 16,
-    borderBottomWidth: 1,
-    borderColor: Colors.WHITE,
-    color: Colors.WHITE,
-    width: 270,
+    backgroundColor: Colors.TRANSGREENLOGOUT,
+    borderRadius: 8,
+    width: 140,
+    height: 40,
+    left: '50%',
+    justifyContent: 'center'
   },
   saveButtonText: {
+    color: Colors.WHITE,
+    fontSize: 15,
+    fontWeight: '500',
+    textAlign: 'center'
+    
+  },
+  textInput: {
+    backgroundColor: Colors.DARKDARKGREEN,
+    borderColor: Colors.BORDERGREEN,
+    borderRadius: 8,
+    borderWidth: 1,
     color: Colors.LIGHTFGREEN,
     fontSize: 16,
-    textDecorationLine: 'underline',
-    fontWeight: '400',
-    shadowColor: Colors.LIGHTFGREEN,
+    marginBottom: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
   },
+  textInputBox: {
+    alignSelf: 'center',
+    marginBottom: 50,
+    width: '80%',
+  },
+  passInputBox: {
+    alignSelf: 'center',
+    marginBottom: 50,
+    width: '80%',
+  },
+  textInputFields:{
+    top: '9%'
+  },
+
+
 });
