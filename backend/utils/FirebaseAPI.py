4c41186310b4be98fbbffb916bad02d737ac105e
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2023-12-15 22:52:55 -0500

    fixed bugs in apis

--- backend/utils/FirebaseAPI.py
+++ backend/utils/FirebaseAPI.py
@@ -1,4 +1,5 @@
 # from typing import Optional
+import os
 from typing import Optional
 import firebase_admin
 from firebase_admin import credentials, auth
@@ -6,7 +7,21 @@ from firebase_admin import credentials, auth
 from models.user import User
 from mongodb_api.carbon_track_db import CarbonTrackDB
 
-cred = credentials.Certificate("secrets.json")
+firebase_cert = {
+  "type": os.environ.get("type"),
+  "project_id": os.environ.get("project_id"),
+  "private_key_id": os.environ.get("private_key_id"),
+  "private_key": os.environ.get("private_key"),
+  "client_email": os.environ.get("client_email"),
+  "client_id": os.environ.get("client_id"),
+  "auth_uri": os.environ.get("auth_uri"),
+  "token_uri": os.environ.get("token_uri"),
+  "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+  "client_x509_cert_url": os.environ.get("client_x509_cert_url"),
+  "universe_domain": os.environ.get("universe_domain")
+}
+
+cred = credentials.Certificate(firebase_cert)
 APP = firebase_admin.initialize_app(cred)
 
 
